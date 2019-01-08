e9e6322ed04392ed34ca65be99f177f8614ca7a7
Marcos Sandoval Calvo <m.sandovalcalvo@gmail.com>
2019-01-08 00:05:07 -0600

    Cleaning up and refactoring

--- app/src/main/java/com/articles/marcos/livedata_viewmodel/View/MainActivity.kt
+++ app/src/main/java/com/articles/marcos/livedata_viewmodel/View/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.articles.marcos.livedata_viewmodel.View
 
 import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.articles.marcos.livedata_viewmodel.R
-import com.articles.marcos.livedata_viewmodel.dao.GenderDao
 import com.articles.marcos.livedata_viewmodel.dataBase.AppDatabase
 import com.articles.marcos.livedata_viewmodel.dataRepository.Repository
-import com.articles.marcos.livedata_viewmodel.entity.Gender
 import com.articles.marcos.livedata_viewmodel.viewModel.MainViewModel
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,7 +19,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         db = AppDatabase.getAppDataBase(context = this)
 
-        val model = ViewModelProviders.of(this, MainViewModel.ViewModelFactory(Repository(db))).get(MainViewModel::class.java)
+        val model = ViewModelProviders
+                .of(this, MainViewModel.ViewModelFactory(Repository(db)))
+                .get(MainViewModel::class.java)
 
         model.genders?.observe(this, Observer {
             var finalString = ""
--- app/src/main/java/com/articles/marcos/livedata_viewmodel/viewModel/MainViewModel.kt
+++ app/src/main/java/com/articles/marcos/livedata_viewmodel/viewModel/MainViewModel.kt
@@ -1,18 +1,12 @@
 package com.articles.marcos.livedata_viewmodel.viewModel
 
-import android.app.Application
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import com.articles.marcos.livedata_viewmodel.dao.GenderDao
-import com.articles.marcos.livedata_viewmodel.dataBase.AppDatabase
 import com.articles.marcos.livedata_viewmodel.dataRepository.Repository
 import com.articles.marcos.livedata_viewmodel.entity.Gender
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainViewModel(private var repository: Repository) : ViewModel() {
     var genders: MutableLiveData<List<Gender>>? = MutableLiveData()
